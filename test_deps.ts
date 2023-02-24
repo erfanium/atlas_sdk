e236d3da43616ad92d54a4f125398dbaeec128a4
GitHub <noreply@github.com>
2023-02-24 09:44:30 +0000

    Update dependencies

--- test_deps.ts
+++ test_deps.ts
@@ -1,2 +1,2 @@
-export { deferred } from "https://deno.land/std@0.175.0/async/deferred.ts";
-export { assertEquals } from "https://deno.land/std@0.175.0/testing/asserts.ts";
+export { deferred } from "https://deno.land/std@0.178.0/async/deferred.ts";
+export { assertEquals } from "https://deno.land/std@0.178.0/testing/asserts.ts";
