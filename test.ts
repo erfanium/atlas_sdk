ea7e8219b5ebed7fadc7b7b5f4b9131d0f91a9e2
Erfanium <erfanshield@outlook.com>
2024-03-24 20:14:23 +0330

    remove usage of deprecated std member

--- test.ts
+++ test.ts
@@ -1,9 +1,9 @@
 // deno-lint-ignore-file require-await
 import { MongoClient, ObjectId, UUID } from "./mod.ts";
-import { assertEquals, deferred } from "./test_deps.ts";
+import { assertEquals } from "./test_deps.ts";
 
 Deno.test("Sample Test", async () => {
-  const fetchMock = deferred<{ url: string; init: RequestInit }>();
+  const fetchMock = Promise.withResolvers<{ url: string; init: RequestInit }>();
 
   const client = new MongoClient({
     endpoint: "https://data.mongodb-api.com/app/data-abc/endpoint/data/v1",
@@ -30,15 +30,15 @@ Deno.test("Sample Test", async () => {
       uuid: new UUID("408ebbdc-2651-4aa4-8298-3aef14e78f7e"),
     });
 
-  const { url, init } = await fetchMock;
+  const { url, init } = await fetchMock.promise;
   assertEquals(
     url,
-    "https://data.mongodb-api.com/app/data-abc/endpoint/data/v1/action/insertOne",
+    "https://data.mongodb-api.com/app/data-abc/endpoint/data/v1/action/insertOne"
   );
   assertEquals(init.method, "POST");
   assertEquals(
     new Headers(init.headers).get("Content-Type"),
-    "application/ejson",
+    "application/ejson"
   );
   assertEquals(new Headers(init.headers).get("api-key"), "API_KEY");
   assertEquals(await new Request(url, init).json(), {
--- test_deps.ts
+++ test_deps.ts
@@ -1,2 +1,1 @@
-export { deferred } from "https://deno.land/std@0.220.1/async/deferred.ts";
 export { assertEquals } from "https://deno.land/std@0.220.1/testing/asserts.ts";
